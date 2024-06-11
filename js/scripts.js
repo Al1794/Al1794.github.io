999d5a2b7e03f4e02eb6ed1d6e356f5be5dedd2e
Al1794 <anisaajike@gmail.com>
2024-06-11 19:48:28 +0100

    Gosh...you better work

--- js/scripts.js
+++ js/scripts.js
@@ -244,4 +244,13 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+    const menuIcon = document.querySelector('.menu-icon');
+    const nav = document.querySelector('nav');
+
+    menuIcon.addEventListener('click', function () {
+        nav.classList.toggle('active');
+    });
+});
+
 
