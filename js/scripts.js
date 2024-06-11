851c1159d39a8a43960097f411be8ce8c3e2b45d
Al1794 <anisaajike@gmail.com>
2024-06-11 15:15:17 +0100

    nnnnnn

--- js/scripts.js
+++ js/scripts.js
@@ -209,11 +209,3 @@ stars.forEach((star, index) => {
     });
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    const menuIcon = document.querySelector('.menu-icon');
-    const nav = document.querySelector('nav');
-
-    menuIcon.addEventListener('click', function () {
-        nav.classList.toggle('active');
-    });
-});
